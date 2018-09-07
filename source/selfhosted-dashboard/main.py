7a5dc178e22f9cdd988caad282528c0d08003188
Simon <simjenner3@gmail.com>
2018-09-07 17:51:45 +0100

    Add redirecat for www domain

--- source/selfhosted-dashboard/main.py
+++ source/selfhosted-dashboard/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 app.config["SERVER_NAME"] = "simonjenner.gq"
@@ -6,3 +6,8 @@ app.config["SERVER_NAME"] = "simonjenner.gq"
 @app.route("/")
 def dashboard():
     return render_template("index.html")
+
+# Redirect www.simonjenner.gq to simonjenner.gq
+@app.route("/", subdomain="www")
+def showStatusPage():
+    return redirect("https://simonjenner.gq/", code=302)
