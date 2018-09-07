0790b43456a1619c53c8c87355b47f4da9a79442
Simon <simjenner3@gmail.com>
2018-09-07 17:54:16 +0100

    Move redirect to nginx

--- source/selfhosted-dashboard/main.py
+++ source/selfhosted-dashboard/main.py
@@ -6,8 +6,3 @@ app.config["SERVER_NAME"] = "simonjenner.gq"
 @app.route("/")
 def dashboard():
     return render_template("index.html")
-
-# Redirect www.simonjenner.gq to simonjenner.gq
-@app.route("/", subdomain="www")
-def showStatusPage():
-    return redirect("https://simonjenner.gq/", code=302)
