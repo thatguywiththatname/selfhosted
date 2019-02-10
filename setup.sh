dbb09a3e5d876bcde651c8bab4f8c797e7e1c5de
thatguywiththatname <simjenner3@gmail.com>
2019-02-10 23:39:45 +0000

    Fix path err

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 # Install everything needed from apt
 sudo apt update
 sudo apt upgrade -y
-sudo apt install nginx fail2ban sendmail ufw
+sudo apt install nginx fail2ban sendmail ufw -y
 
 # Install certbot
-sudo apt-get install software-properties-common
+sudo apt-get install software-properties-common -y
 sudo add-apt-repository universe
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update
-sudo apt-get install certbot python-certbot-nginx 
+sudo apt-get install certbot python-certbot-nginx -y
 
 # Setup UFW
 sudo ufw default deny incoming
@@ -19,8 +19,8 @@ sudo ufw allow "Nginx HTTPS"
 sudo ufw enable
 
 # Move service files to correct locations
-sudo cp -R /opt/selfhosted/setup/services/fail2ban/. /etc/fail2ban/.
-sudo cp -R /opt/selfhosted/setup/services/nginx/. /etc/nginx/sites-available/.
+sudo cp -R /opt/selfhosted/services/fail2ban/. /etc/fail2ban/.
+sudo cp -R /opt/selfhosted/services/nginx/. /etc/nginx/sites-available/.
 
 # Link nginx sites to be enabled
 sudo ln -s /etc/nginx/sites-available/selfhosted-webserver /etc/nginx/sites-enabled
