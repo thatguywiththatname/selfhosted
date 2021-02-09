a59a2ee1ddcd6ebc7a7b3a98c391602b6387db44
thatguywiththatname <simjenner3@gmail.com>
2021-02-09 00:46:52 +0000

    Add ttn-receiver

---
+++ ttn-receiver/README.md
@@ -0,0 +1,5 @@
+# PyonAir TTN Recevier
+
+Stuff for my final year project. Receives from TTN and writes to file.
+
+Included in Caddyfile but not setup script.
---
+++ ttn-receiver/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+// Note: Data is written to /data, mount that volume to get it.
+
+func writeFile(w http.ResponseWriter, r *http.Request) {
+    dataBytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	f, err := os.OpenFile("/data/data.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer f.Close()
+
+	if _, err := f.Write(dataBytes); err != nil {
+		log.Println(err)
+		return
+	}
+
+	if _, err := f.WriteString("\n"); err != nil {
+		log.Println(err)
+	}
+
+	log.Println("New data written")
+}
+
+func main() {
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", writeFile)
+    log.Println("Started")
+    err := http.ListenAndServe(":8080", mux)
+    log.Fatal(err)
+}
