2361a8969153600ec3d7278a1a166ac8e546a657
thatguywiththatname <simjenner3@gmail.com>
2021-02-09 19:00:35 +0000

    Update script

--- ttn-receiver/out-to-csv.py
+++ ttn-receiver/out-to-csv.py
@@ -4,15 +4,19 @@
 
 import json
 import sys
+import dateutil.parser
 
 print("time,pm1_pm10,pm1_pm25,pm2_pm10,pm2_pm25,temperature,humidity")
 with open(sys.argv[1], "r") as f:
     for line in f.readlines():
         data = json.loads(line)
         p = data["payload_fields"]
-        time = data["metadata"]["time"]
+        time = dateutil.parser.isoparse(data["metadata"]["time"])
+        timeH = time.house
+        timeM = time.minute
+        timeS = time.second
         temp = int(p["temperature"]) / 10
         humidity = int(p["humidity"]) / 10
         print(
-            f'{time},{p["PM1_PM10"]},{p["PM1_PM25"]},{p["PM2_PM10"]},{p["PM2_PM25"]},{temp},{humidity}'
+            f'{timeH}:{timeM}:{timeS},{p["PM1_PM10"]},{p["PM1_PM25"]},{p["PM2_PM10"]},{p["PM2_PM25"]},{temp},{humidity}'
         )
